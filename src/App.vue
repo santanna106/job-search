5f7a9f5de63bc08c9fd809680ae0b0baf7d0682b
Gabriel Andrade <gabriel.santana@gmail.com>
2023-02-02 04:21:46 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <MainNav />
+  <h1 class="text-3xl font-bold underline">Hello World</h1>
+</template>
+<script>
+import MainNav from "@/components/MainNav.vue";
+export default {
+  components: {
+    MainNav,
+  },
+};
+</script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">Bobo Careers</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+};
+</script>
